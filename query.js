933587ea8ce40424aa41e421a89f52f5550fa803
tanapruk <trust@nextzy.com>
2018-05-10 12:27:03 +0700

    Add log extracter

--- query.js
+++ query.js
@@ -1,11 +1,18 @@
 const Eth = require('web3-eth')
 const eth = new Eth(`https://ropsten.infura.io/${process.env.INFURA_API_KEY}`)
 const { abi } = require('./generated_abi')
-
-//the transaction that created
+const abiDecoder = require('abi-decoder')
+//the transaction that create contract
+abiDecoder.addABI(abi)
 const transactionHash = '0xd3c13a9ac0d950f54d92acd6b2bea02c875de42262d25e22b12331e154030f6a'
 
-const transaction = eth.getTransaction(transactionHash)
-const transactionReceipt = eth.getTransactionReceipt(transactionHash)
 
+
+eth.getTransactionReceipt(transactionHash)
+  .then(result => {
+    console.log('=============================logs============================')
+    console.log(result.logs)
+    console.log('==============================converted to==========================')
+    console.log(JSON.stringify(abiDecoder.decodeLogs(result.logs), null, 2))
+  })
 
